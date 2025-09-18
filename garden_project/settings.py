bc2ebc679ff04c222a59cd5f8a83ac2b26c1fb16
nebalcony <nebalcony@gmail.com>
2025-09-18 07:15:23 +0300

    change settings.py

--- garden_project/settings.py
+++ garden_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i(g9pl+(r8coo5!_5*_@x2%973ow!n793isw$4q8q(m&0-=4n('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
